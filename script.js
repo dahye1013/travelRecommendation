05014328c7e5e4f042469d36993326f022ad6ead
dahye shin <forlecture1013@gmail.com>
2024-09-29 14:38:40 +0900

    fix: add main tag for layout, clear after search

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
 const images = {
   sydney:
     "https://images.unsplash.com/photo-1506973035872-a4ec16b8e8d9?w=400&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8c3lkbmV5fGVufDB8fDB8fHww",
+  melbourne:
+    "https://images.unsplash.com/photo-1545044846-351ba102b6d5?w=400&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8bWVsYm91cm5lfGVufDB8fDB8fHww",
   tokyo:
     "https://images.unsplash.com/photo-1540959733332-eab4deabeeaf?w=400&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8dG9reW98ZW58MHx8MHx8fDA%3D",
+  kyoto:
+    "https://images.unsplash.com/photo-1512692723619-8b3e68365c9c?w=400&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8a3lvdG98ZW58MHx8MHx8fDA%3D",
   rio: "https://images.unsplash.com/photo-1483729558449-99ef09a8c325?w=400&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nnx8cmlvfGVufDB8fDB8fHww",
   ["angkor-wak"]:
     "https://plus.unsplash.com/premium_photo-1661963188432-5de8a11f21a7?w=400&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8YW5na29yJTIwd2F0fGVufDB8fDB8fHww",
@@ -12,6 +16,12 @@ const images = {
     "https://images.unsplash.com/photo-1532408840957-031d8034aeef?w=400&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8Ym9yYV9ib3JhfGVufDB8fDB8fHww",
   copacabana:
     "https://images.unsplash.com/photo-1643400813604-b3877e0c4db9?w=400&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTR8fGNvcGFjYWJhbmF8ZW58MHx8MHx8fDA%3D",
+  "angkor-wat":
+    "https://plus.unsplash.com/premium_photo-1661963188432-5de8a11f21a7?w=400&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8YW5na29yJTIwd2F0fGVufDB8fDB8fHww",
+  "taj-mahal":
+    "https://plus.unsplash.com/premium_photo-1661885523029-fc960a2bb4f3?w=400&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8dGFqJTIwbWFoYWx8ZW58MHx8MHx8fDA%3D",
+  "sao-paulo":
+    "https://plus.unsplash.com/premium_photo-1683888229109-17cb0975af20?w=400&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8c2FvJTIwcGF1bG98ZW58MHx8MHx8fDA%3D",
 };
 
 const destinationList = document.getElementById("destination-list");
@@ -30,7 +40,9 @@ const destination_card = (
   info = { id: "", description: "", imageUrl: "", name: "" },
 ) => `
     <img src=${
-      images[Object.keys(images).find((key) => info.imageUrl.includes(key))]
+      images[
+        Object.keys(images).find((key) => info.imageUrl?.includes?.(key))
+      ] ?? ""
     } alt="Sydney, Australia" class="destination-image">
     <div class="destination-content">
         <h3>${info.name}</h3>
@@ -48,8 +60,6 @@ const drawTravelRecommendations = (recommendations = []) => {
   }
 
   recommendations.forEach((recommendation) => {
-    console.log(recommendation);
-
     const card = document.createElement("div");
     card.classList.add("destination-card");
     card.innerHTML = destination_card(recommendation);
@@ -59,12 +69,48 @@ const drawTravelRecommendations = (recommendations = []) => {
 
 const searchHandler = async (e) => {
   e.preventDefault();
+  destinationList.innerHTML = "";
   const keyword = document.getElementById("search").value;
-  const recommendations = await getTravelRecommend();
-  const [result] = Object.entries(recommendations)
-    .filter(([key]) => key === keyword.toLowerCase())
-    .map(([_key, value]) => value);
-  drawTravelRecommendations(result);
+
+  function flattenData(data) {
+    let flattenedArray = [];
+
+    if (data.countries) {
+      flattenedArray = data.countries.reduce((acc, country) => {
+        const cities = country.cities.map((city) => ({
+          name: city.name,
+          imageUrl: city.imageUrl,
+          description: city.description,
+          category: "countries",
+        }));
+        return acc.concat(cities);
+      }, []);
+    }
+
+    Object.keys(data).forEach((category) => {
+      if (category !== "countries") {
+        const items = data[category].map((item) => ({
+          name: item.name,
+          imageUrl: item.imageUrl,
+          description: item.description,
+          category: category,
+        }));
+        flattenedArray = flattenedArray.concat(items);
+      }
+    });
+    console.log(flattenedArray);
+
+    return flattenedArray;
+  }
+
+  const results = await getTravelRecommend().then((data) => {
+    return flattenData(data).filter(
+      (item) => item.category === keyword.toLowerCase(),
+    );
+  });
+  console.log(results, results.length);
+
+  drawTravelRecommendations(results);
 };
 
 const clearHandler = () => {
